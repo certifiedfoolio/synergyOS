a1edee914bcc0ef62bd6b23143f6eec1dd06e6bb
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-26 14:31:32 -0800

    feat: prepare for stable

---
+++ files/scripts/install-budgie.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+dnf5 -y group install budgie-desktop budgie-desktop-apps
--- modules/groupinstall/groupinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-get_json_array INSTALL_GROUPS 'try .["groups"][]' "$1"
-for THING in "${INSTALL_GROUPS[@]}"; do
-    dnf5 -y group install "$THING"
-done
